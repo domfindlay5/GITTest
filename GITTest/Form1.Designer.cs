f882f77016f7de8ba61bd4d90d32f3303a739959
P16170444 <P16170444@my365.dmu.ac.uk>
2018-01-30 09:42:20 +0000

    Changed to Test

--- GITTest/Form1.Designer.cs
+++ GITTest/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -43,15 +42,6 @@
             this.label1.Text = "Git clone and pull TEST";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(10, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = ".";
-            // 
             // btnTest
             // 
             this.btnTest.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -59,7 +49,7 @@
             this.btnTest.Name = "btnTest";
             this.btnTest.Size = new System.Drawing.Size(129, 34);
             this.btnTest.TabIndex = 2;
-            this.btnTest.Text = "Branch Test";
+            this.btnTest.Text = "Test";
             this.btnTest.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -68,7 +58,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.btnTest);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,7 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnTest;
     }
 }
